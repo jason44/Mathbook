f994a66f06ec4b9750cd433f4081cfd28c7d16a6
Jason Soegondo <jasonsoegondo@gmail.com>
2023-02-28 13:07:22 -0800

    fixed buffer overflow

--- VDraw/src/main.c
+++ VDraw/src/main.c
@@ -74,8 +74,8 @@ typedef struct Polygon {
 inline void vec2_to_image_coordinates(double image_width, double image_height, 
 	vec2 *verticies, const size_t vertex_count)	
 {
-	image_width /= 2;
-	image_height /= 2;
+	image_width /= 2.0;
+	image_height /= 2.0;
 	for (size_t i = 0; i < vertex_count; i++) {
 		verticies[i].x = verticies[i].x + image_width;
 		verticies[i].y = (verticies[i].y + image_height) -1;
@@ -99,7 +99,9 @@ void vdraw_create(VDrawContext *ctx)
 	ctx->cr = cairo_create(ctx->surface);
 	cairo_set_source_rgba(ctx->cr, 1.0, 1.0, 1.0, 1.0);
 	//cairo_set_line_cap(ctx->cr, CAIRO_LINE_CAP_ROUND);
-	cairo_set_line_cap(ctx->cr, CAIRO_LINE_CAP_SQUARE);
+	cairo_set_line_cap(ctx->cr, CAIRO_LINE_CAP_ROUND);
+	cairo_set_line_join(ctx->cr, CAIRO_LINE_JOIN_ROUND);
+	cairo_set_fill_rule(ctx->cr, CAIRO_FILL_RULE_WINDING);
 	cairo_paint(ctx->cr);	
 }
 
@@ -147,30 +149,42 @@ void vdraw_polygon(VDrawContext *ctx, Polygon *poly)
 
 	// close the polygon even if its already closed
 	vec2 buf[vertex_count+1];
-	vertices = buf;
 
 	if (vertices[0].x != vertices[vertex_count-1].x &&
 		vertices[0].y != vertices[vertex_count-1].y) {
 		// TODO: Check if realloc is faster than using memcpy 
-		memcpy(vertices, poly->vertices, sizeof(vec2)*(vertex_count));
-		vertices[vertex_count] = (vec2){vertices[0].x, vertices[0].y};
+		printf("vertex_count: %lu\n",vertex_count);
+		memcpy(buf, vertices, sizeof(vec2)*(vertex_count));
+		buf[vertex_count] = (vec2){vertices[0].x, vertices[0].y};
+		vertices = buf;
+		++vertex_count;
+		puts("HELLO");
 	}
 
+	for (size_t k = 0; k < vertex_count; k++) printf("(%f, %f)\n", vertices[k].x, vertices[k].y);	
+	puts("--------------");
+
 	cairo_move_to(cr, vertices[0].x, vertices[0].y);
-	for (size_t i = 1; i < poly->vertex_count; i++) {
+	for (size_t i = 1; i < vertex_count; i++) {
 		cairo_line_to(cr, vertices[i].x, vertices[i].y);			
 	}
 
-	cairo_stroke(cr);
+	// preserve the path so cairo knows what to fill
 	if (poly->fill) {
-		cairo_set_source_rgba(cr, c[0], c[1], c[2], 0.1);
+		cairo_stroke_preserve(cr);
+		cairo_set_source_rgba(cr, c[0], c[1], c[2], 0.2);
 		cairo_fill(cr);
-	}
+	} else cairo_stroke(cr);
 }
 
-inline double vec2_dot(vec2 u, vec2 v) 
+inline double vec2_dot(const vec2 u, const vec2 v) 
 {
 	return (u.x * v.x) + (u.y * v.y);
+}
+
+inline double vec2_cross(const vec2 u, const vec2 v)
+{
+	return u.x;		
 }
 
 /*********** FREE THE ALLOCATED ARRAY ***********/
@@ -190,15 +204,16 @@ int main(int argc, char** argv)
 	const size_t vertex_count = 3;
 	vec2 vertices[] = {
 		{0.0, 0.0},
-		{5.0, 5.0},
-		{5.0, 2.0}
+		{4.0, 4.0},
+		{4.0, -2.0}
 	};
 
 	Polygon poly = {
 		.vertices = vertices,
 		.vertex_count = vertex_count,
-		.lw = 0.2,
-		.color = {0.1, 0.1, 0.1, 1.0}
+		.lw = 0.1,
+		.color = {0.1, 0.1, 0.1, 1.0},
+		.fill = true
 	};
 	
 	VDrawContext ctx = {
@@ -208,8 +223,11 @@ int main(int argc, char** argv)
 	};
 	vdraw_create(&ctx);
 	vdraw_polygon(&ctx, &poly);
+	puts("POLYGON");
 	vdraw_save(&ctx);
+	puts("SAVE");
 	vdraw_destroy(&ctx);
+	puts("DESTROY");
 
 	return 0;
 }
