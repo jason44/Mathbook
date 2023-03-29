dfebd2fd5603e8fe06a4fc17fdef36d7e2eab31b
jason <jasonsoegondo@gmail.com>
2023-03-28 19:19:21 -0700

    cleaned up target directory

--- GRender/target/debug/GRender.d
+++
@@ -1,1 +0,0 @@
-/home/jason/Desktop/Mathbook/GRender/target/debug/GRender: /home/jason/Desktop/Mathbook/GRender/src/main.rs
--- GRender/target/debug/deps/GRender-329ec9f64b99d2e2.d
+++
@@ -1,5 +0,0 @@
-/home/jason/Desktop/Mathbook/GRender/target/debug/deps/GRender-329ec9f64b99d2e2: src/main.rs
-
-/home/jason/Desktop/Mathbook/GRender/target/debug/deps/GRender-329ec9f64b99d2e2.d: src/main.rs
-
-src/main.rs:
